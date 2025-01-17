cdcc87a54b72f88787dff58ba50974ab8c22ef11
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2025-01-17 13:07:34 +0100

    js for dropdown and styling

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -7,16 +7,21 @@ class DropDown extends HTMLElement {
         const template = document.getElementById('drop-down').content.cloneNode(true);
         let open = 0;
         const showButton = template.querySelector("article button");
-        showButton.addEventListener("click", () => {
-            if (open) {
-                showButton.nextElementSibling.classList.add("hidden")
-                showButton.querySelector("svg").classList.remove("rotate-180")
-            } else {
-                showButton.nextElementSibling.classList.remove("hidden") 
-                showButton.querySelector("svg").classList.add("rotate-180")
-            }
-            open = !open;
+        const dl = template.querySelector("article dl");
+        [showButton, dl].forEach(e => {
+            e.addEventListener("click", () => {
+                if (open) {
+                    showButton.nextElementSibling.classList.add("hidden")
+                    showButton.querySelector("svg").classList.remove("rotate-180")
+                } else {
+                    showButton.nextElementSibling.classList.remove("hidden") 
+                    showButton.querySelector("svg").classList.add("rotate-180")
+                }
+                open = !open;
+            });
         });
+        showButton
+        
         this.appendChild(template);
     }
 }
