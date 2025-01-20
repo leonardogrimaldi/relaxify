68cb85adc7fae4ad97e5185f1c58009d3fabfeaf
Garcia Rufer Azael <azael.garciarufer@studio.unibo.it>
2025-01-20 12:10:14 +0100

    Fixed nav

--- src/template/header.php
+++ src/template/header.php
@@ -11,16 +11,16 @@
     <!-- Title -->
     <a class="text-4xl py-5 font-bold w-full flex" href="./index.php">
         <div class="invisible w-[5%]"></div>
-        <h1 class="w-full">Relaxify</h1>
+        <h1 class="w-full text-center">RELAXIFY</h1>
     </a>
 
     <!-- Icone preferiti login carrello -->
     <nav class="py-5 min-w-max">
-        <ul class="flex flex-row items-center justify-end gap-x-1">
-            <li>
+        <ul class="flex flex-row items-center justify-end gap-x-2">
+            <li class="">
                 <a href="./preferiti.php">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-9">
-                        <title>Cuore</title>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-1/2 h-9">
+                        <title>Preferiti</title>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                     </svg>
                     <span class="sr-only">Lista preferiti</span>
@@ -28,8 +28,8 @@
             </li>
             <li>
                 <a href="./login.php">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-9">
-                        <title>Omino</title>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-1/2 h-9">
+                        <title>Login</title>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
                     <span class="sr-only">Area personale</span>
@@ -37,7 +37,7 @@
             </li>
             <li class="mr-3">
                 <a href="./cart.php">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-9">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-1/2 h-9">
                         <title>Carrello</title>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                     </svg>
@@ -108,10 +108,12 @@
     </div>
 </header>
 
-    <menu class="w-screen flex gap-x-3 px-3 overflow-x-auto whitespace-nowrap">
-        <?php
-        foreach($templateParams["categorie"] as $categoria): ?>
-        <!-- Assicurarsi di mettere ml auto al primo elemento e mr auto all'ultimo per un funzionamento desktop centrato -->
-        <li class="py-2 flex-none"><button type="button" class="w-32 text-pink-400 hover:text-pink-700 font-semibold border-4 border-pink-200 py-1 rounded-full"><?php echo $categoria["nome"]; ?></button></li>
+    <menu class="w-screen flex gap-x-3 px-3 overflow-x-auto whitespace-nowrap justify-center">
+        <?php foreach ($templateParams["categorie"] as $categoria): ?>
+            <li class="py-2 flex-none">
+                <button type="button" class="w-32 text-pink-400 hover:text-pink-700 font-semibold border-4 border-pink-200 py-1 rounded-full">
+                    <?php echo $categoria["nome"]; ?>
+                </button>
+            </li>
         <?php endforeach; ?>
     </menu>
