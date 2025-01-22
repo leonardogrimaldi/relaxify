21a8a95d462f9b384eb1ac9c1a3316099f279aee
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2025-01-22 16:25:40 +0100

    fixed navbar

--- src/template/header.php
+++ src/template/header.php
@@ -1,25 +1,27 @@
 <header class="bg-pink-300 text-white flex items-center w-full">
     <!-- Button sidebar -->
-    <button onclick="toggleBarra()" class="px-3 flex items-center text-white hover:bg-teal-600">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <title>Tab</title>
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
-        <span class="sr-only">Apri la barra laterale</span>
-    </button>
+     <div class="sm:w-1/3">
+        <button onclick="toggleBarra()" class="px-3 flex items-center text-white hover:bg-teal-600">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <title>Tab</title>
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+            <span class="sr-only">Apri la barra laterale</span>
+        </button>
+     </div>
+    
 
     <!-- Title -->
-    <a class="text-4xl py-5 font-bold w-full flex" href="./index.php">
-        <div class="invisible w-[5%]"></div>
-        <h1 class="w-full text-center">RELAXIFY</h1>
+    <a class="text-4xl py-5 font-bold w-full flex sm:w-1/3" href="./index.php">
+        <h1 class="w-full text-center text-3xl">RELAXIFY</h1>
     </a>
 
     <!-- Icone preferiti login carrello -->
-    <nav class="py-5 w-full">
-        <ul class="flex flex-row items-center justify-end">
+    <nav class="py-5 max-w-min sm:w-1/3 sm:max-w-full">
+        <ul class="flex flex-row items-center justify-end gap-x-1 md:gap-x-4">
             <li>
                 <a href="./preferiti.php">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-1/2 h-9">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-9">
                         <title>Preferiti</title>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                     </svg>
@@ -29,7 +31,7 @@
             <li>
                 <?php if(isUserLoggedIn()): ?>
                 <a href="./profilo.php">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-1/2 h-9">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-9">
                         <title>Login</title>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
@@ -37,7 +39,7 @@
                 </a>
                 <?php else : ?>
                 <a href="./login.php">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-1/2 h-9">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-9">
                         <title>Login</title>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
@@ -46,8 +48,8 @@
                 <?php endif; ?>
             </li>
             <li class="mr-3">
-                <a href="./cart.php">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-1/2 h-9">
+                <a href="./carrello.php">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-9">
                         <title>Carrello</title>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                     </svg>
