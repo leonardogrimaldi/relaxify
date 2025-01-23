d8ab6da5b61457d2eee42754a42d66f345784b4e
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2025-01-23 17:28:58 +0100

    buttons

--- src/template/favorites.php
+++ src/template/favorites.php
@@ -1,10 +1,8 @@
 <div class="grow flex flex-col lg:flex-row">
-    <section class="w-full grow flex flex-col items-center">
+    <section class="w-full grow flex flex-col items-center bg-white rounded-lg">
         <h2 class="text-center text-xl py-3">Preferiti</h2>
-        <article class="flex flex-row mx-2 items-center sm:w-2/3 sm:mx-0 h-28">
-            <div class="w-1/3 h-full flex justify-end">
-                <img class="aspect-square rounded" src="resources/img/cube.jpg" alt="Palla Antistress">
-            </div>
+        <article class="flex flex-row items-center sm:w-2/3 sm:mx-0 h-32 w-full max-w-xs">
+            <img class="aspect-square rounded" height="100" width="100" src="resources/img/cube.jpg" alt="Palla Antistress">
             <div class="flex flex-col h-full grow ml-2">
                 <hgroup>
                     <h3 class="text-lg">Palla antistress</h3>
@@ -12,18 +10,23 @@
                 </hgroup>
                 <p class="mt-2">Descrizione</p>
             </div>
-            <div class="flex flex-col h-full justify-around items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                </svg>
+            <div class="flex flex-col h-full justify-around items-center mr-2">
+                <button type="button" class="h-full w-full flex justify-center items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6">
+                        <title>Preferiti</title>
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                    </svg>
+                    <span class="sr-only">Aggiungi/Rimuovi dai preferiti</span>
+                </button>
                 <data value="15.00">15.00€</data>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                </svg>
+                <button type="button" class="h-full w-full flex justify-center items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                    </svg>
+                    <span class="sr-only">Aggiungi al carrello</span>
+                </button>
             </div>
         </article>
     </section>
