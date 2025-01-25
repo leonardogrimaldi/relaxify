6f45b7be5d6df44c96b8b55d57cdcec189a8fc6d
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2025-01-25 14:13:53 +0100

    working colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,15 @@ module.exports = {
     extend: {
       colors : {
         //customcolors
-        darkBlue: '#112D4E',
-        blue: '#3F72AF',
-        lightBlue: '#DBE2EF',
-        white: '#F9F7F7',
+        'darkBlue': '#112D4E',
+        'blue': '#3F72AF',
+        'lightBlue': '#DBE2EF',
+        'white': '#F9F7F7',
 
-        darkPink: '#FF90BC',
-        pink: '#FFC0D9',
-        yellow: '#F9F9EO',
-        skyBlue: '#8ACDD7',
+        'darkPink': '#FF90BC',
+        'pink': '#FFC0D9',
+        'yellow': '#F9F9EO',
+        'skyBlue': '#8ACDD7',
       },
     },
   },
