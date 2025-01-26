5b2ad8f4a2ce3b767fda5d934b478be5957200dd
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2025-01-26 19:53:35 +0100

    notifications protected

--- src/notifications.php
+++ src/notifications.php
@@ -8,13 +8,23 @@ function testInput($data)
 }
 require_once("bootstrap.php");
 header('Content-Type: application/json');
-if (isset($_GET['notificaID'])) {
-    $notificaID = testInput($_GET['notificaID']);
-    $dbh->readNotification($notificaID);
-    echo json_encode($_GET['notificaID']);
-    exit;
+if(isUserLoggedIn()){
+    if (isset($_GET['notificaID'])) {
+        $notificaID = testInput($_GET['notificaID']);
+        $dbh->readNotification($notificaID);
+        echo json_encode($_GET['notificaID']);
+        exit;
+    }
+    if ($_SESSION["tipo"] == 0) {
+        $notifications = $dbh->getNotifications($user = "utente");
+        echo json_encode($notifications);
+    } elseif ($_SESSION["tipo"] == 1) {
+        $notifications = $dbh->getNotifications($user = "admin");
+        echo json_encode($notifications);
+    }
+} else {
+    $text = "Utente non loggato";
+    echo json_encode($text);
 }
-$notifications = $dbh->getNotifications($user = "utente");
-echo json_encode($notifications);
 exit;
 ?>
