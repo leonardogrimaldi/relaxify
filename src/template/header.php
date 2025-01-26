5486c782afe795ba8add61d5550fd41d56d8f302
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2025-01-26 21:30:44 +0100

    added dynamic header

--- src/template/header.php
+++ src/template/header.php
@@ -17,8 +17,9 @@
     </a>
 
     <!-- Icone preferiti login carrello -->
-    <nav class="py-5 max-w-min sm:w-1/3 sm:max-w-full">
+    <nav class="py-5 max-w-min sm:w-1/3 sm:max-w-full mr-3">
         <ul class="flex flex-row items-center justify-end gap-x-1 md:gap-x-4">
+            <?php if(!isAdminLoggedIn()) : ?>
             <li>
                 <a href="./preferiti.php">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-9">
@@ -28,6 +29,7 @@
                     <span class="sr-only">Lista preferiti</span>
                 </a>
             </li>
+            <?php endif; ?>
             <li>
                 <a href="./login.php">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-9">
@@ -37,7 +39,8 @@
                     <span class="sr-only">Area personale</span>
                 </a>
             </li>
-            <li class="mr-3">
+            <?php if(!isAdminLoggedIn()) : ?>
+            <li>
                 <a href="./carrello.php">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-9">
                         <title>Carrello</title>
@@ -46,6 +49,7 @@
                     <span class="sr-only">Carrello</span>
                 </a>
             </li>
+            <?php endif; ?>
         </ul>
     </nav>
 
@@ -63,24 +67,28 @@
                     </svg>
                     <span>Listino Prodotti</span>
                 </a>
-                <a href="preferiti.php" class="flex items-center text-gray-800 hover:text-blue">
+                <?php if(!isAdminLoggedIn()) : ?>
+                    <a href="preferiti.php" class="flex items-center text-gray-800 hover:text-blue">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                     </svg>
                     <span>Preferiti</span>
-                </a>
+                    </a>
+                <?php endif; ?>
                 <a href="login.php" class="flex items-center text-gray-800 hover:text-blue">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
                     <span>Area Personale</span>
                 </a>
+                <?php if(!isAdminLoggedIn()) : ?>
                 <a href="carrello.php" class="flex items-center text-gray-800 hover:text-blue">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                     </svg>
                     <span>Carrello</span>
                 </a>
+                <?php endif; ?>
             </div>
 
             <hr class="border-gray-300">
