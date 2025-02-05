62e352a0b6b1fa3bd79d7295a784210a52d859ff
Garcia Rufer Azael <azael.garciarufer@studio.unibo.it>
2025-02-05 11:00:19 +0100

    Button

--- src/template/favorites.php
+++ src/template/favorites.php
@@ -18,7 +18,13 @@
                     </div>
                     <div class="flex flex-col h-full justify-around items-center mr-2">
                         <data value="<?php echo $prodottopreferito["prezzo"] ?>"><?php echo $prodottopreferito["prezzo"] ?>€</data>
-                        <button class="addToCart mt-2 px-4 py-2 bg-blue text-white rounded-lg hover:bg-blue-600" data-prodotto-id="<?php echo $prodottopreferito["prodottoID"]; ?>"">Aggiungi al carrello</button>
+                        <button class="addToCart mt-2 px-4 py-2 bg-blue text-white rounded-lg hover:bg-darkBlue" data-prodotto-id="<?php echo $prodottopreferito["prodottoID"]; ?>"">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <title>Carrello</title>
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                        </svg>
+                        <span class="sr-only">Aggiungi al carrello</span>    
+                        </button>
                     </div>
                 </article> 
                 </a> 
