72db14b569a0dd363c51647aa3aaa0a69731e590
Garcia Rufer Azael <azael.garciarufer@studio.unibo.it>
2025-02-05 16:25:33 +0100

    Small change

--- src/template/suggestions.php
+++ src/template/suggestions.php
@@ -4,28 +4,24 @@
         <?php foreach($templateParams["prodottisuggeriti"] as $prodottosuggerito): ?>
         <article class="bg-white rounded-lg flex flex-col overflow-hidden shadow-md">
             <img class="h-full aspect-square" onclick="window.location.href='prodotto.php?id=<?php echo $prodottosuggerito['prodottoID'] ?>'" src="<?php echo IMG_ROOT . $prodottosuggerito["immagine"]; ?>"
-                alt="<?php echo $prodottosuggerito["nome"]; ?>">
-                <section onclick="window.location.href='prodotto.php?id=<?php echo $prodottosuggerito['prodottoID'] ?>'" 
-    class="text-center flex flex-col" 
-    itemscope itemtype="https://schema.org/Product">
+            alt="<?php echo $prodottosuggerito["nome"]; ?>">
+            <section onclick="window.location.href='prodotto.php?id=<?php echo $prodottosuggerito['prodottoID'] ?>'" 
+                class="text-center flex flex-col" 
+                itemscope itemtype="https://schema.org/Product">
+                <h2 class="text-lg font-medium text-gray-700" itemprop="name">
+                    <?php echo $prodottosuggerito["nome"]; ?>
+                </h2>
+                <div class="flex flex-row justify-center items-center gap-x-2 py-2" 
+                itemprop="offers" itemscope itemtype="https://schema.org/Offer">
+                    <div>
+                        <data itemprop="price" value="<?php echo $prodottosuggerito["prezzo"]; ?>">
+                            <?php echo $prodottosuggerito["prezzo"]; ?> €
+                        </data>
+                        <meta itemprop="priceCurrency" content="EUR" />
+                    </div>
+                </div>
 
-    <h2 class="text-lg font-medium text-gray-700" itemprop="name">
-        <?php echo $prodottosuggerito["nome"]; ?>
-    </h2>
-
-    <div class="flex flex-row justify-center items-center gap-x-2 py-2" 
-        itemprop="offers" itemscope itemtype="https://schema.org/Offer">
-        
-        <div>
-            <data itemprop="price" value="<?php echo $prodottosuggerito["prezzo"]; ?>">
-                <?php echo $prodottosuggerito["prezzo"]; ?> €
-            </data>
-            <meta itemprop="priceCurrency" content="EUR" />
-        </div>
-    </div>
-
-</section>
-
+            </section>
         </article>
         <?php endforeach; ?>
     </div>
