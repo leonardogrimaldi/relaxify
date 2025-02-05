6faf41dcd42e45887988dff61f1febd2279fe579
Garcia Rufer Azael <azael.garciarufer@studio.unibo.it>
2025-02-05 16:23:04 +0100

    Acc

--- src/template/favorites.php
+++ src/template/favorites.php
@@ -7,18 +7,19 @@
             <p class="p-4 text-center">Non hai prodotti preferiti. <a href="./index.php" class="font-medium text-darkBlue underline hover:no-underline">Torna alla homepage</a> per scegliere un articolo preferito premendo sul pulsante 'Cuore' oppure selezionane uno dai suggerimenti!</p> 
             <?php else:?>
             <?php foreach ($templateParams["prodottipreferiti"] as $prodottopreferito): ?>
-                <a href="prodotto.php?id=<?php echo $prodottopreferito['prodottoID'] ?>">
                 <article class="flex flex-row items-center m-2 mt-1 rounded-sm h-32">
-                    <img class="aspect-square rounded" height="100" width="100" src="<?php echo IMG_ROOT . $prodottopreferito["immagine"] ?>" alt="<?php echo $prodottopreferito["nome"] ?>">
-                    <div class="flex flex-col h-full grow ml-2">
-                        <hgroup>
-                            <h3 class="text-lg"><?php echo $prodottopreferito["nome"] ?></h3>
-                        </hgroup>
-                        <p class="mt-2 line-clamp-3"><?php echo $prodottopreferito["descrizione"] ?></p>
-                    </div>
+                    <a href="prodotto.php?id=<?php echo $prodottopreferito['prodottoID'] ?>">
+                    <img class="aspect-square rounded" height="300" width="300" src="<?php echo IMG_ROOT . $prodottopreferito["immagine"] ?>" alt="<?php echo $prodottopreferito["nome"] ?>">
+                        <div class="flex flex-col h-full grow ml-2">
+                            <hgroup>
+                                <h3 class="text-lg"><?php echo $prodottopreferito["nome"] ?></h3>
+                            </hgroup>
+                            <p class="mt-2 line-clamp-3"><?php echo $prodottopreferito["descrizione"] ?></p>
+                        </div>
+                    </a> 
                     <div class="flex flex-col h-full justify-around items-center mr-2">
                         <data value="<?php echo $prodottopreferito["prezzo"] ?>"><strong><?php echo $prodottopreferito["prezzo"] ?>€</strong></data>
-                        <button class="addToCart mt-2 px-4 py-2 bg-blue text-white rounded-lg hover:bg-darkBlue" data-prodotto-id="<?php echo $prodottopreferito["prodottoID"]; ?>"">
+                        <button class="addToCart mt-2 px-4 py-2 bg-blue text-white rounded-lg hover:bg-darkBlue" data-prodotto-id="<?php echo $prodottopreferito["prodottoID"]; ?>">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <title>Carrello</title>
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
@@ -27,7 +28,7 @@
                         </button>
                     </div>
                 </article> 
-                </a> 
+                
             <?php endforeach; ?>
             <?php endif;?>
         </div>
--- src/template/suggestions.php
+++ src/template/suggestions.php
@@ -5,15 +5,27 @@
         <article class="bg-white rounded-lg flex flex-col overflow-hidden shadow-md">
             <img class="h-full aspect-square" onclick="window.location.href='prodotto.php?id=<?php echo $prodottosuggerito['prodottoID'] ?>'" src="<?php echo IMG_ROOT . $prodottosuggerito["immagine"]; ?>"
                 alt="<?php echo $prodottosuggerito["nome"]; ?>">
-            <section onclick="window.location.href='prodotto.php?id=<?php echo $prodottosuggerito['prodottoID'] ?>'" class="text-center flex flex-col">
-                <h2 class="text-lg font-medium text-gray-700"><?php echo $prodottosuggerito["nome"]; ?></h2>
-                <div class="flex flex-row justify-center items-center gap-x-2 py-2">
-                    <div>
-                        <data value="<?php echo $prodottosuggerito["prezzo"]; ?>"><?php echo $prodottosuggerito["prezzo"]; ?></data>
-                        <meta itemprop="priceCurrency" content="EUR" />€
-                    </div>
-                </div>
-            </section>
+                <section onclick="window.location.href='prodotto.php?id=<?php echo $prodottosuggerito['prodottoID'] ?>'" 
+    class="text-center flex flex-col" 
+    itemscope itemtype="https://schema.org/Product">
+
+    <h2 class="text-lg font-medium text-gray-700" itemprop="name">
+        <?php echo $prodottosuggerito["nome"]; ?>
+    </h2>
+
+    <div class="flex flex-row justify-center items-center gap-x-2 py-2" 
+        itemprop="offers" itemscope itemtype="https://schema.org/Offer">
+        
+        <div>
+            <data itemprop="price" value="<?php echo $prodottosuggerito["prezzo"]; ?>">
+                <?php echo $prodottosuggerito["prezzo"]; ?> €
+            </data>
+            <meta itemprop="priceCurrency" content="EUR" />
+        </div>
+    </div>
+
+</section>
+
         </article>
         <?php endforeach; ?>
     </div>
