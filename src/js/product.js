eda404f54af53240cc7cac669ac2d5328f6b2364
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2025-02-06 11:27:39 +0100

    fixed cartMessage on mobile for product page

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,14 @@
 // Handle fixed div overlapping footer
 const footer = document.querySelector("footer");
 const fixedDiv = document.getElementById('fixed-div');
+const cartMessage = document.getElementById('cartMessage');
 const setFooterMargin = () => {
     if (this.document.body.clientWidth >= 640) {
         footer.style.marginBottom = '0px';
+        cartMessage.style.marginBottom = '0px';
     } else {
         footer.style.marginBottom = `${fixedDiv.clientHeight}px`;
+        cartMessage.style.marginBottom = `${fixedDiv.clientHeight}px`;
     }
 }
 setFooterMargin();
